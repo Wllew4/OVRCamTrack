0ffde83b52ddb882c8f7171c5b78e29f73d4784a
Wllew4 <soupsepicgamesbusiness@gmail.com>
2021-04-13 12:52:51 -0400

    addded unity project

---
+++ OVRCT_client/Assets/Pipe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.IO.Pipes;
+
+public class Pipe : MonoBehaviour
+{
+    private NamedPipeClientStream client = new NamedPipeClientStream("OVRCTPipe");
+    StreamReader reader;
+    StreamWriter writer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        client.Connect();
+        reader = new StreamReader(client);
+        writer = new StreamWriter(client);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        writer.WriteLine("Hello World!");
+        writer.Flush();
+    }
+}
